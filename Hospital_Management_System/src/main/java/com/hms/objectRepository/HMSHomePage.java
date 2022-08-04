236fd28ac06f9be31a2301f7528caf1eb86a5a31
Indrajitm96 <moharanaindrajit@gmail.com>
2022-08-04 11:11:36 +0530

    homePage & LoginPage

--- Hospital_Management_System/src/main/java/com/hms/objectRepository/HMSHomePage.java
+++ Hospital_Management_System/src/main/java/com/hms/objectRepository/HMSHomePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 public class HMSHomePage {
 
 	//initialization
+	
 	public HMSHomePage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
