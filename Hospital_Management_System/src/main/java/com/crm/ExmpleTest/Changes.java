a58727ff56987da6357a32685ebbcb58d55139fc
Indrajitm96 <moharanaindrajit@gmail.com>
2022-08-04 08:55:30 +0530

    hiii

---
+++ Hospital_Management_System/src/main/java/com/crm/ExmpleTest/Changes.java
@@ -0,0 +1,11 @@
+package com.crm.ExmpleTest;
+
+public class Changes {
+
+	public static void main(String[] args) {
+		System.out.println("good morning");
+		System.out.println("hav a nic day");
+
+	}
+
+}
